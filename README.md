1f7aee60b393c0a65528db5e0bcf046cc4cca708
Marco Ravicini <marco.ravicini@bbv.ch>
2017-06-28 14:34:17 +0200

    Add initial project files.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Description
+
+Bootstrap project for extreme fast kickoff. C# .NET Core
+
+- Library
+- Unit Tests Library
+- Spec Tests Library
+
+## Run tests
+
+```
+dotnet tool restore
+dotnet paket restore
+dotnet test
+```
+
+[![Build status](https://ci.appveyor.com/api/projects/status/hrm954v1lt856erf/branch/master?svg=true)](https://ci.appveyor.com/project/MarcoRavicini/tddacademycore/branch/master)
---
+++ TddAcademy.Facts/FooFact.cs
@@ -0,0 +1,30 @@
+﻿namespace TddAcademy.Facts
+{
+    using FakeItEasy;
+    using FluentAssertions;
+    using Xunit;
+    
+    public class FooTest
+    {
+        private readonly Foo testee;
+        private readonly IBar barFake;
+
+        public FooTest()
+        {
+            this.barFake = A.Fake<IBar>();
+            this.testee = new Foo(this.barFake);
+        }
+
+        [Fact]
+        public void Say()
+        {
+            const string BarReturn = "fake";
+            const string FooReturn = "foo";
+            A.CallTo(() => this.barFake.Say()).Returns(BarReturn);
+
+            var actual = this.testee.Say();
+
+            actual.Should().Be(FooReturn + BarReturn);
+        }
+    }
+}
